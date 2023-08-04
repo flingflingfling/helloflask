165af14134eef4ca697c97ca175763ec9f155626
flingflingfling <admin@777521.xyz>
2023-08-04 07:30:55 +0000

    add template for html

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 from flask import url_for
 
 app = Flask(__name__)
 
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 @app.route('/')
-@app.route('/index')
+@app.route('/main')
 @app.route('/home')
 def hello():
     return 'welcome to my chatbox!'
@@ -14,6 +28,10 @@ def hello():
 def user_page(name):
     return f'This is User {escape(name)}  page'
 
+@app.route('/index')
+def index():
+    return render_template('index.html', name=name, movies=movies)
+     
 @app.route('/test')
 def test_url():
     print(url_for('test_url'))
